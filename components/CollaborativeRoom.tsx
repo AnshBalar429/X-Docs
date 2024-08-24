94f4ebbf420f3f591cf8f77b252ac5903379ed02
Ansh Balar <anshbalar2910@gmail.com>
2024-08-24 14:53:56 +0530

    Added CollaborativeRoom component

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,22 +4,27 @@ import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense';
 import React from 'react'
 import { Editor } from './editor/Editor';
 import Header from './Header';
+import ActiveCollaborators from './ActiveCollaborators';
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
     return (
-        <RoomProvider id="my-room">
+        <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading...</div>}>
                 <div className='collaborative-room'>
                     <Header>
                         <div className='flex w-fit items-center justify-center gap-2'>
                             <p className='document-title'>This is a fake title</p>
                         </div>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
+                        <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
+                            <ActiveCollaborators />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
+
                     </Header>
                     <Editor />
                 </div>
